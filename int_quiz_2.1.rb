e12180fce2ee626f7b50d0bbf46591ff3503af07
Kira McLean <kiraemclean@gmail.com>
2015-09-20 14:39:58 -0400

    Complete intermediate quiz 2

---
+++ int_quiz_2.1.rb
@@ -0,0 +1,16 @@
+munsters = { 
+  "Herman" => { "age" => 32, "gender" => "male" }, 
+  "Lily" => { "age" => 30, "gender" => "female" }, 
+  "Grandpa" => { "age" => 402, "gender" => "male" }, 
+  "Eddie" => { "age" => 10, "gender" => "male" } 
+}
+
+
+sum = 0
+munsters.each do |name, info|
+    sum += info["age"] if info["gender"] == "male"
+end
+
+puts sum
+
+
---
+++ int_quiz_2.4.rb
@@ -0,0 +1,5 @@
+s = "Humpty Dumpty sat on a wall"
+
+new_s = s.split.reverse!.join(" ") + "."
+
+p new_s
